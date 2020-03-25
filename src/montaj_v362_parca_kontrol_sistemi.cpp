4ee1f4ff24a4c6c9925e0565a40183899a07bb51
msezer <mustafacanersezer@gmail.com>
2020-03-25 16:51:30 +0300

    baska 1 şey

--- src/montaj_v362_parca_kontrol_sistemi.cpp
+++ src/montaj_v362_parca_kontrol_sistemi.cpp
@@ -174,13 +174,11 @@ int main(int argc, char const *argv[])
 			b = 0;
 		}
 		cv::circle(goruntu_RGB, cv::Point(60,1040), 40, CV_RGB(r,g,b), -1);
-		//cv::rectangle(goruntu_RGB,cv::Point(40,1050),cv::Point(700,1050),CV_RGB(r,g,b),3);
+
 		cv::putText(goruntu_RGB, sonucText, cv::Point(150, 1050), cv::FONT_HERSHEY_DUPLEX, 2.0, CV_RGB(r, g, b), 2);
 
 		goruntu_kaydet(goruntu_RGB);
 
-		//imshow("goruntu_RGB",goruntu_RGB);
-		//waitKey(1);
 
 		send(conn_fd, &ack, 1, 0);
 		syslog(LOG_INFO, "%s(): acknowledge: '%c' gonderildi.\n", __func__, ack);
@@ -209,9 +207,6 @@ string goruntuyu_isle(cv::Mat goruntu)
 	ROI_height_1 = 180;
 	CamROI_1 = Rect(ROI_x_1, ROI_y_1, ROI_width_1, ROI_height_1);
 
-	//string full_dizin = goruntu_ismi; // burasi duzelecektir.
-	//printf("full_dizin=[%s]\n", full_dizin.c_str());
-
 	int Goruntu_genislik = capture.get(CAP_PROP_FRAME_WIDTH);
 	int Goruntu_yukseklik = capture.get(CAP_PROP_FRAME_HEIGHT);
 
@@ -238,6 +233,7 @@ string goruntuyu_isle(cv::Mat goruntu)
 
 	if (CamFrame_RGB.empty())
 	{
+		int sonuc;
 		syslog(LOG_INFO, "Goruntu okuma hatasi / Network baglanti sorunu\n");
 		capture.release();
 		sleep(4);
@@ -255,11 +251,6 @@ string goruntuyu_isle(cv::Mat goruntu)
 	Rect RectangleToDraw2(x1_baslangic, y1_baslangic, 10, 10);
 	rectangle(CamFrame_RGB_crop_1, RectangleToDraw2.tl(), RectangleToDraw2.br(), Scalar(0, 0, 255), 2, 8, 0);
 
-	//imwrite("/home/mpolat/montaj_cita_tanima/build/CamFrame_RGB_crop_1.bmp", CamFrame_RGB_crop_1);
-	//sleep(1);
-	//printf("Kaydedildi.\n");
-	//break;
-
 	Mat sutun1(CamFrame_Gry_crop_1.rows, 1, CV_8UC1);
 	Mat sutun2(CamFrame_Gry_crop_1.rows, 1, CV_8UC1);
 
@@ -269,7 +260,6 @@ string goruntuyu_isle(cv::Mat goruntu)
 		{
 			sutun1.at<unsigned char>(r) = CamFrame_Gry_crop_1.at<unsigned char>(r, c);
 			toplam1 = toplam1 + CamFrame_Gry_crop_1.at<unsigned char>(r, c);
-			//printf("grey.at<unsigned char>(r[%d], c[%d])==[%d]\n",r,c,grey.at<unsigned char>(r, c));
 		}
 	}
 	ortalama1 = toplam1 / ((x1_bitis - x1_baslangic) * (y1_bitis - y1_baslangic));
@@ -281,7 +271,6 @@ string goruntuyu_isle(cv::Mat goruntu)
 		{
 			sutun2.at<unsigned char>(r) = CamFrame_Gry_crop_1.at<unsigned char>(r, c);
 			toplam2 = toplam2 + CamFrame_Gry_crop_1.at<unsigned char>(r, c);
-			//printf("grey.at<unsigned char>(r[%d], c[%d])==[%d]\n",r,c,grey.at<unsigned char>(r, c));
 		}
 	}
 	ortalama2 = toplam2 / ((x2_bitis - x2_baslangic) * (y2_bitis - y2_baslangic));
@@ -289,25 +278,13 @@ string goruntuyu_isle(cv::Mat goruntu)
 	syslog(LOG_INFO, "%s(): Ortalamalar: Ortalama1= [%d],  Ortalama2= [%d].", __func__, ortalama1,ortalama2);
 	if (abs(ortalama1 - ortalama2) < 20)
 	{
-		//printf("Par�alar Ayn� Tondad�r.\n");
 		myText = "Ayni";
 	}
 	else
 	{
-		//printf("Par�alar Farkl� Tondad�r.\n");
-		//putText(img = CamFrame_Gry_crop_1, text = "Hii", org = (30,30), fontFace = FONT_HERSHEY_DUPLEX, fontScale = 3, color = (0, 255, 0))
-		//putText(CamFrame_Gry_crop_1, "Hii", cvPoint(30,30),FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200,200,250), 1, CV_AA);
 		myText = "Farkli";
 	}
-	/*cv::putText(CamFrame_RGB_crop_1, //target image
-				myText,				 //text
-				cv::Point(30, 30),   //top-left position
-				cv::FONT_HERSHEY_DUPLEX,
-				1.0,
-				CV_RGB(0, 255, 0), //font color
-				2);*/
-	//imshow("MONTAJ FINAL :)", CamFrame_RGB_crop_1);
-	//waitKey(1);
+	
 
 	toplam1 = 0;
 	toplam2 = 0;
